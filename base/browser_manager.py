3504a3d137626aa615008dfc871d16d39d6e504f
Paul-Marchenko <pavel.marchenko.6583@gmail.com>
2018-11-18 17:54:00 +0200

    browser configs

--- base/browser_manager.py
+++ base/browser_manager.py
@@ -1,12 +1,29 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
+from pages.urls import main_url
+
+
 class WebDriverManager:
 
     def __init__(self, browser):
         self.browser = browser
 
     def get_browser_type(self):
-        webdriver.Chrome(executable_path=ChromeDriverManager().install())
+        if self.browser == "iexplorer":
+            driver = webdriver.Ie()
+        elif self.browser == "firefox":
+            driver = webdriver.Firefox()
+        elif self.browser == "safari":
+            driver = webdriver.Safari()
+        elif self.browser == "chrome":
+            webdriver.Chrome(executable_path=ChromeDriverManager().install())
+        else:
+            raise ValueError("Incorrect browser name")
+        driver.implicitly_wait(3)
+        # driver.maximize_window()
+        driver.get(main_url())
+        return driver
+
 
 
---
+++ pages/urls.py
@@ -0,0 +1,4 @@
+def main_url(path=None):
+    if path is not None:
+        return f'https://dou.ua/{path}'
+    return 'https://dou.ua/'
